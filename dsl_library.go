56717ee19b509301b1107295ab6467c85d646167
Lukasz Szymik <l.szymik@saas-ag.com>
2022-05-18 14:37:05 +0200

    Sync between machines

--- dsl_library.go
+++ dsl_library.go
@@ -1,13 +1,23 @@
 package dsl_library
 
 import (
+	"fmt"
 	"go.uber.org/cadence/workflow"
+	"sort"
+	"strings"
 	"time"
 
 	"go.uber.org/zap"
 )
 
+const (
+	fieldSeparator = "."
+	asterisk       = "*"
+)
+
 type (
+	Payload map[string]any
+
 	Workflow struct {
 		Id          string    `json:"id"`
 		Description string    `json:"description,omitempty"`
@@ -46,7 +56,7 @@ type (
 	}
 )
 
-func DSLWorkflow(ctx workflow.Context, dslWorkflow Workflow) ([]byte, error) {
+func DSLWorkflow(ctx workflow.Context, dslWorkflow Workflow, p Payload) ([]byte, error) {
 	ao := workflow.ActivityOptions{
 		ScheduleToStartTimeout: time.Minute,
 		StartToCloseTimeout:    time.Minute,
@@ -138,4 +148,117 @@ func executeAsync(exe Executable, ctx workflow.Context) workflow.Future {
 		settable.Set(nil, err)
 	})
 	return future
+}
+
+func ProcessMap(p Payload, m Payload) (Payload, error) {
+	if m == nil {
+		return p, nil
+	}
+
+	payload := make(Payload)
+	metadata := make(Payload)
+
+	flattenMap(nil, p, payload)
+	flattenMap(nil, m, metadata)
+
+	o := make(map[string]any)
+	for k, v := range payload {
+		if meta, ok := metadata[k]; ok {
+			if include, ok := meta.(bool); ok {
+				if !include {
+					continue
+				} else {
+					o[k] = v
+				}
+			}
+		} else {
+			idx := strings.LastIndex(k, fieldSeparator)
+			if idx != -1 {
+				search := fmt.Sprintf("%s.*", k[:idx])
+				if _, ok := metadata[search]; ok {
+					o[k] = v
+				}
+			} else {
+				if _, ok := metadata[asterisk]; ok {
+					if meta, ok := metadata[k]; ok {
+						if include, ok := meta.(bool); ok {
+							if !include {
+								continue
+							} else {
+								o[k] = v
+							}
+						}
+					} else {
+						o[k] = v
+					}
+				}
+			}
+		}
+	}
+	return unflattenMap(o), nil
+}
+
+func flattenMap(prefix *string, m Payload, r Payload) {
+	for k, v := range m {
+		switch v.(type) {
+		case Payload:
+			x, _ := v.(Payload)
+			key := getKeyName(prefix, k)
+			flattenMap(&key, x, r)
+		default:
+			key := getKeyName(prefix, k)
+			r[key] = v
+		}
+	}
+}
+
+func getKeyName(prefix *string, k string) string {
+	if prefix != nil {
+		return fmt.Sprintf("%s.%s", *prefix, k)
+	}
+	return k
+}
+
+func unflattenMap(m Payload) Payload {
+	sk := getSortedKeys(m)
+	res := make(Payload)
+
+	for _, k := range sk {
+		if isComposed(k) {
+			im, lk := getMapEntry(res, k)
+			im[lk] = m[k]
+		} else {
+			res[k] = m[k]
+		}
+	}
+	return res
+}
+
+func isComposed(s string) bool {
+	return strings.Contains(s, fieldSeparator)
+}
+
+func getSortedKeys(m Payload) []string {
+	keys := make([]string, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	return keys
+}
+
+func getMapEntry(m Payload, k string) (Payload, string) {
+	curMap := m
+	var nk string
+	for ; isComposed(k); k = k[strings.Index(k, fieldSeparator)+1:] {
+		nk = k[:strings.Index(k, fieldSeparator)]
+		if innerMap, ok := curMap[nk]; ok {
+			curMap = innerMap.(Payload)
+		} else {
+			newMap := make(Payload)
+			curMap[nk] = newMap
+			curMap = newMap
+		}
+	}
+	return curMap, k
 }
